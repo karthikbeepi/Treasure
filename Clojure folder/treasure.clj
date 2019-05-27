04770dfacade9f0e207f316f5cd0be972ab979b7
BeePi <karthikbeepi@gmail.com>
2019-05-27 12:02:02 -0400

    Its working for now

--- Clojure folder/treasure.clj
+++ Clojure folder/treasure.clj
@@ -19,7 +19,7 @@
       )))
 
 ;Program begins from here
-(readFile "map1.txt")
+(readFile "map.txt")
 (println "NoR :" noOfRows "NoC: " noOfCol)
 (def oneString (apply str oneString))
 ; (println oneString)
@@ -64,35 +64,110 @@
 
 ; (println (checkBoundary -1 ))
 ; Find the path to @
-(defn findPath [ i ]
-  (def res (atom false))
-  false
+; (defn findPath [ i ]
+;   false
+;   (if (checkBoundary i)
+;     false
+;     (if (= (str (aget arr i)) "@")
+;       true
+;       (do
+;         (aset arr i "!")
+;         ; (def up (- i noOfCol))
+;         ; (def down (+ i noOfCol))
+;         ; (def right (getRight i))
+;         ; (def left (getLeft i))
+;         (def upRes (findPath (getUp i)))
+;         (def leftRes (findPath (getLeft i)))
+;         (def rightRes (findPath (getRight i)))
+;         (def downRes (findPath (getDown i)))
+;         ; (def res (or upRes downRes leftRes rightRes))
+;         (println "Came here" i upRes leftRes downRes rightRes)
+;         ; (println (findPath 1))
+;         (if (or upRes downRes leftRes rightRes)
+;           (do
+;             (aset arr i "+")
+;             true
+;             ))
+;         false)
+;       )
+;     )
+;   )
+
+(defn checkReached [i]
   (if (checkBoundary i)
     false
     (if (= (str (aget arr i)) "@")
       true
+      false))
+  )
+
+
+(defn neighbourReached [i]
+  (if (checkBoundary i)
+    (do
+      (println "Out of Reach" i)
+      false)
+    (if (checkReached (getDown i))
       (do
-        (aset arr i "!")
-        ; (def up (- i noOfCol))
-        ; (def down (+ i noOfCol))
-        ; (def right (getRight i))
-        ; (def left (getLeft i))
-        (def upRes (findPath (getUp i)))
-        (def leftRes (findPath (getLeft i)))
-        (def rightRes (findPath (getRight i)))
-        (def downRes (findPath (getDown i)))
-        ; (def res (or upRes downRes leftRes rightRes))
-        (println "Came here" i upRes leftRes downRes rightRes res)
-        ; (println (findPath 1))
-        (if (or upRes downRes leftRes rightRes)
+        (println "Reached " i "Down")
+        true)
+      (if (checkReached (getRight i))
+        (do
+          (println "Reached " i "Right")
+          true)
+        (if (checkReached (getLeft i))
+          (do
+            (println "Reached " i "Left")
+            true)
+          (if (checkReached (getUp i))
+            (do
+              (println "Reached " i "Up")
+              true)
+            (if (checkReached i)
+              (do
+                (println "Reached " i)
+                true)
+              false
+              )
+            )
+          )
+        )))
+  )
+
+;  (println (checkReached 8))
+;  (println (neighbourReached 0))
+
+(defn searchPath [i]
+  (if (checkBoundary i)
+    false
+    (if (checkReached i)
+      true
+      (do  (aset arr i "!")
+(if (neighbourReached i)
+  (do
+    (aset arr i "+")
+    true)
+  (if (searchPath (getDown i))
+    (do
+      (aset arr i "+")
+      true)
+    (if (searchPath (getLeft i))
+      (do
+        (aset arr i "+")
+        true)
+      (if (searchPath (getUp i))
+        (do
+          (aset arr i "+")
+          true)
+        (if (searchPath (getRight i))
           (do
             (aset arr i "+")
-            true
-            ))
-        false)
+            true)
+          false))))) )
       )
     )
   )
-(println (findPath 0))
+
+(println (searchPath 0))
 (println (apply str arr))
 
